82a7fd60e6abd0867b7cb062f73f58e2eb74e4f9
bebroch <cawapog@yandex.ru>
2024-02-25 22:01:20 +0300

    init: project

---
+++ server/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { ConversionModule } from "./conversion/conversion.module";
+
+@Module({
+    imports: [AuthModule, ConversionModule],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
