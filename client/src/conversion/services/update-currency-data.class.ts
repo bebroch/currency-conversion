09649760b9174c04e921f476d934bb3b5d512270
bebroch <cawapog@yandex.ru>
2024-03-08 22:49:06 +0300

    feat: clean update currency class from comments

--- client/src/conversion/services/update-currency-data.class.ts
+++ client/src/conversion/services/update-currency-data.class.ts
@@ -5,34 +5,6 @@ export default class UpdateCurrencyData extends CurrencyApi {
     public async updateData(
         data: DefaultCurrencyType,
     ): Promise<["from" | "to", data: string] | undefined> {
-        // const updateData = (key: string, count: string) => {
-        //     return ["to", count]
-
-        //     const defaultData = {
-        //         from: {
-        //             ...data.from,
-        //             fetchUpdate: true,
-        //         },
-        //         to: {
-        //             ...data.to,
-        //             fetchUpdate: true,
-        //         },
-        //     } as DefaultCurrencyType
-
-        //     switch (key) {
-        //         case "from":
-        //             console.log({ ...defaultData, from: { ...defaultData.from, count } })
-        //             setData({ ...defaultData, from: { ...defaultData.from, count } })
-        //             return ["from", count]
-        //             break
-        //         case "to":
-        //             console.log({ ...defaultData, to: { ...defaultData.to, count } })
-        //             setData({ ...defaultData, to: { ...defaultData.to, count } })
-        //             return ["to", count]
-        //             break
-        //     }
-        // }
-
         if (!data.from.fetchUpdate) {
             const response = await super.getCurrencyConversion({
                 fromCount: data.from.count,
@@ -40,8 +12,6 @@ export default class UpdateCurrencyData extends CurrencyApi {
                 toCurrency: data.to.currency,
             })
 
-            // console.log(1, data.from)
-            // updateData("to", response.value.toString())
             return ["to", response.value.toString()]
         } else if (!data.to.fetchUpdate) {
             const response = await super.getCurrencyConversion({
@@ -50,8 +20,6 @@ export default class UpdateCurrencyData extends CurrencyApi {
                 toCurrency: data.from.currency,
             })
 
-            // console.log(2)
-            // updateData("from", response.value.toString())
             return ["from", response.value.toString()]
         }
 
