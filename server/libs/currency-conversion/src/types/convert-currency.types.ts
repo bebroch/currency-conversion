f215f8997df5e91ce707349edc2bed5cd58799ba
bebroch <cawapog@yandex.ru>
2024-03-08 19:01:27 +0300

    fix: import

--- server/libs/currency-conversion/src/types/convert-currency.types.ts
+++ server/libs/currency-conversion/src/types/convert-currency.types.ts
@@ -1,4 +1,4 @@
-import { CurrencyEnum } from "@currency-conversion/currency-conversion/apis/currency-api/enums/currency.enums"
+import { CurrencyEnum } from "../apis/currency-api/enums/currency.enums"
 
 export type ConvertCurrencyType = {
     fromCurrency: CurrencyEnum
